49a3fb3a9dfdc8a020e11da6591536517a371410
Muhd Ayman Haque <131044870+aymanhaque@users.noreply.github.com>
2025-10-06 11:14:31 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Testing CI/CD(october6)"
+    return "Testing CI/CD(october6- attempt 2)"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
