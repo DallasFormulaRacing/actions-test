2929ee7f76c52f7ea25414b86e43419bcb51c81d
aymanhaque <muhd.ayman.haque@gmail.com>
2025-10-06 21:53:16 -0500

    sentry test

--- app.py
+++ app.py
@@ -1,15 +1,20 @@
+import sentry_sdk
 from flask import Flask
+
+sentry_sdk.init(
+    dsn="https://b91d300f29870176c8f05b114d8ca558@o4510128361963520.ingest.us.sentry.io/4510145722056704",
+    # Add data like request headers and IP for users,
+    # see https://docs.sentry.io/platforms/python/data-management/data-collected/ for more info
+    send_default_pii=True,
+)
+sentry_sdk.set_tag("service", "flask-app")
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Testing CI/CD(october6- attempt new"
-
-@app.route("/errorTest")
-def error():
-    raise Exception("Error test")
+def hello_world():
+    1/0  # raises an error
+    return "<p>Hello, World!</p>"
 
 if __name__ == "__main__":
-    print("test 1")
     app.run(host="0.0.0.0", port=5000, debug=True)
