ada9fb6b6ac3a3d60ed657900ea08eca4878697a
Rei Shibatani <147196816+reiiiiiiiiii@users.noreply.github.com>
2025-11-07 17:40:41 -0600

    Change hello_world response to 'Demo'

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def trigger_error():
 
 @app.route("/")
 def hello_world():
-    return "Test Success! CICD now pushes to dallasformularacing Dockerhub"
+    return "Demo"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
