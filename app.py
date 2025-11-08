f20d3b7cec5a77daede45ad5488d170012b0eaf7
Rei Shibatani <147196816+reiiiiiiiiii@users.noreply.github.com>
2025-11-07 18:16:27 -0600

    Change response from 'Demo' to 'Demo 2'

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def trigger_error():
 
 @app.route("/")
 def hello_world():
-    return "Demo"
+    return "Demo 2"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
