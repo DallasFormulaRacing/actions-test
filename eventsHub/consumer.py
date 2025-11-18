5a9fb4a50549df0e6e150a15367eaaf92527d705
aymanhaque <muhd.ayman.haque@gmail.com>
2025-11-18 16:12:35 -0600

    events hub consumer gets correct json string

--- eventsHub/consumer.py
+++ eventsHub/consumer.py
@@ -2,15 +2,23 @@
 import asyncio
 from azure.eventhub.aio import EventHubConsumerClient
 import os
+import json
 from dotenv import load_dotenv
 load_dotenv()
 
+
+
 CONNECTION_STR = os.getenv("EVENTSHUB_CONSUMER_STRING")
 EVENT_HUB_NAME = os.getenv("EVENTSHUB_NAME")
-CONSUMER_GROUP = "$Default"
+CONSUMER_GROUP = "carsensordashbaord"
 
 async def on_event(partition_context, event):
-    print(f"Received event from partition {partition_context.partition_id}: {event.body_as_str()}")
+    # Example json string;
+    # {'event': {'event_type': 'temp', 'data': [{'time': '2025-11-18T22:11:11.574241+00:00Z', 'sensor_id': 1, 'data': 80.44}]}}
+    data = json.loads(event.body_as_str())
+
+    print(f"Received event from partition {partition_context.partition_id}\n")
+    print(f"{data}\n\n")
     await partition_context.update_checkpoint(event)
 
 async def main():
@@ -23,7 +31,7 @@ async def main():
     async with client:
         await client.receive(
             on_event=on_event,
-            starting_position="-1"  # "-1" reads from beginning
+            starting_position="@latest"
         )
 
 if __name__ == "__main__":
