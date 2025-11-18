fd5e473ce550274b2707781212f8ce4dbc169817
aymanhaque <muhd.ayman.haque@gmail.com>
2025-11-18 16:12:35 -0600

    events hub consumer gets correct json string

--- eventsHub/sender.py
+++ eventsHub/sender.py
@@ -1,35 +1,8 @@
-# from azure.eventhub import EventHubProducerClient, EventData
-# import os
-# from dotenv import load_dotenv
-# load_dotenv()
-
-# CONNECTION_STR = os.getenv("EVENTSHUB_CONNECTION_STRING")
-# EVENT_HUB_NAME = os.getenv("EVENTSHUB_NAME")
-# def main():
-
-#     print(CONNECTION_STR)
-#     producer = EventHubProducerClient.from_connection_string(
-#         conn_str=CONNECTION_STR,
-#         eventhub_name=EVENT_HUB_NAME
-#     )
-
-#     # Create a batch
-#     event_data_batch = producer.create_batch()
-#     event_data_batch.add(EventData("Hello from Python!"))
-#     event_data_batch.add(EventData("Second message"))
-
-#     # Send
-#     producer.send_batch(event_data_batch)
-#     producer.close()
-#     print("Sent events.")
-
-# if __name__ == "__main__":
-#     main()
-
-
 import asyncio
-
+import datetime
 import os
+import random
+import json
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -39,10 +12,10 @@ from azure.eventhub.aio import EventHubProducerClient
 EVENT_HUB_CONNECTION_STR = os.getenv("EVENTSHUB_CONNECTION_STRING")
 EVENT_HUB_NAME = os.getenv("EVENTSHUB_NAME")
 
+
+
 async def run():
-    # Create a producer client to send messages to the event hub.
-    # Specify a connection string to your event hubs namespace and
-    # the event hub name.
+
     producer = EventHubProducerClient.from_connection_string(
         conn_str=EVENT_HUB_CONNECTION_STR, eventhub_name=EVENT_HUB_NAME
     )
@@ -52,9 +25,24 @@ async def run():
 
         # Add events to the batch.
         for i in range(2):
-            event_data_batch.add(EventData(f"event {i} "))
+            current_time = datetime.datetime.now(datetime.UTC).isoformat() + "Z"
+            random_temp = 70 + random.random() * 30
+
+            message_data = {
+                "event": {
+                    "event_type": "temp",
+                    "data": [
+                        {
+                            "time": current_time,
+                            "sensor_id": 1,
+                            "data": round(random_temp, 2),
+                        }
+                    ],
+                }
+            }
+
+            event_data_batch.add(EventData(json.dumps(message_data)))
         
-        # Send the batch of events to the event hub.
         await producer.send_batch(event_data_batch)
 
 asyncio.run(run())
