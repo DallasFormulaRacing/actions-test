dd70ab7ee7c5ebf4e825260aba9a39760b70ecec
aymanhaque <muhd.ayman.haque@gmail.com>
2025-11-20 19:57:45 -0600

    some changes

--- frontend/src/components/app-sidebar.tsx
+++ frontend/src/components/app-sidebar.tsx
@@ -131,7 +131,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
               className={`data-[slot=sidebar-menu-button]:p-1.5!`}
             >
               <a href="#">
-                <IconInnerShadowTop className="!size-5" />
+                <IconInnerShadowTop className="size-5!" />
                 <span className="text-base font-semibold">Acme Inc.</span>
               </a>
             </SidebarMenuButton>
